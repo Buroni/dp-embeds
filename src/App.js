d3c5b31665cd110a01e464099c3cd626952f36ef
Jake Browning <jake.reid.browning@gmail.com>
2021-07-07 11:50:57 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+
+function App() {
+  return (
+   <div className={"container"}>
+    <div className={"content"}>
+      <div className={"item"}>
+        <iframe src="http://localhost:8090/u/admin/reports/vega-report-unresponsive/embed/" width="100%" height="540px" frameBorder="0">Iframe not supported.</iframe>
+      </div>
+      <div className={"item"}>
+        <iframe src="http://localhost:8090/u/admin/reports/vega-report-responsive/embed/" width="100%" height="540px" frameBorder="0">Iframe not supported.</iframe>
+      </div>
+      <div className={"item"}>
+        <iframe src="http://localhost:8090/u/admin/reports/vega-report-bindings/embed/" width="100%" height="540px" frameBorder="0">Iframe not supported.</iframe>
+      </div>
+      <div className={"item"}>
+        <iframe src="http://localhost:8090/u/admin/reports/plotly-report-unresponsive/embed/" width="100%" height="540px" frameBorder="0">Iframe not supported.</iframe>
+      </div>
+      <div className={"item"}>
+        <iframe src="http://localhost:8090/u/admin/reports/plotly-report-responsive/embed/" width="100%" height="540px" frameBorder="0">Iframe not supported.</iframe>
+      </div>
+      <div className={"item"}>
+        <iframe src="http://localhost:8090/u/admin/reports/bokeh-report-unresponsive/embed/" width="100%" height="540px" frameBorder="0">Iframe not supported.</iframe>
+      </div>
+      <div className={"item"}>
+        <iframe src="http://localhost:8090/u/admin/reports/bokeh-report-responsive/embed/" width="100%" height="540px" frameBorder="0">Iframe not supported.</iframe>
+      </div>
+      <div className={"item"}>
+        <iframe src="http://localhost:8090/u/admin/reports/mpl-report-unresponsive/embed/" width="100%" height="540px" frameBorder="0">Iframe not supported.</iframe>
+      </div>
+      <div className={"item"}>
+        <iframe src="http://localhost:8090/u/admin/reports/mpl-report-responsive/embed/" width="100%" height="540px" frameBorder="0">Iframe not supported.</iframe>
+      </div>
+      <div className={"item"}>
+        <iframe src="http://localhost:8090/u/admin/reports/multi-report-responsive/embed/" width="100%" height="540px" frameBorder="0">Iframe not supported.</iframe>
+      </div>
+      <div className={"item"}>
+        <iframe src="http://localhost:8090/u/admin/reports/demo-report/embed/" width="100%" height="540px" frameBorder="0">Iframe not supported.</iframe>
+      </div>
+      <div className={"item"}>
+        <iframe src="http://localhost:8090/u/admin/reports/table-report/embed/" width="100%" height="540px" frameBorder="0">Iframe not supported.</iframe>
+      </div>
+      <div className={"item"}>
+        <iframe src="http://localhost:8090/u/admin/reports/datatable-report/embed/" width="100%" height="540px" frameBorder="0">Iframe not supported.</iframe>
+      </div>
+    </div>
+   </div>
+  );
+}
+
+export default App;
